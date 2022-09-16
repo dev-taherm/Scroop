00cb9a1a496809b8092bb70186311f76c4b8e142
dev-taherm <tahermahram0@gmail.com>
2022-09-16 11:49:02 -0900

    static

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
   },
   images: {
     domains: ["firebasestorage.googleapis.com"],
-    unoptimized: true,
+    loader: "static",
+  
   },
   eslint: {
     ignoreDuringBuilds: true,
