7b5608f36317c6c31089e28ec58ea0f802ef73c1
dev-taherm <tahermahram0@gmail.com>
2022-09-17 08:11:00 -0900

    fovicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,39 +4,39 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="title" content="Tiptop" />
+        <meta name="title" content="Scroop" />
         <meta name="description" content="Online Shopping for Women" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Tiptop" />
-        <meta property="og:description" content="Online Shopping for Men" />
+        <meta property="og:title" content="Scroop" />
+        <meta property="og:description" content="Online Shopping for Women" />
         <meta
           property="og:image"
-          content="https://user-images.githubusercontent.com/48406108/158584944-2fedf004-1425-4aa7-b1ee-46977c3beb19.png"
+          content=""
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://tiptopstore.vercel.app" />
-        <meta name="twitter:title" content="Tiptop" />
-        <meta name="twitter:description" content="Online Shopping for Men" />
+        <meta name="twitter:url" content="" />
+        <meta name="twitter:title" content="Scroop" />
+        <meta name="twitter:description" content="Online Shopping for Women" />
         <meta
           name="twitter:image"
-          content="https://user-images.githubusercontent.com/48406108/158584944-2fedf004-1425-4aa7-b1ee-46977c3beb19.png"
+          content=""
         />
         <link
-          rel="apple-touch-icon"
+          rel="./apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="./apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="./favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="./favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
