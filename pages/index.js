299ebb4224b074e8d9b209beec77784e39185079
dev-taherm <tahermahram0@gmail.com>
2022-09-29 11:19:50 -0900

    ihih

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,34 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import styled from 'styled-components';
-import { Landing } from '../assets/icons';
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Banner from '../sections/banner';
+import Services from '../sections/services';
+import ExcitingFeatures from '../sections/exciting-features';
+import WorkHard from '../sections/work-hard';
+import UltimateFeatures from '../sections/ultimate-features';
+import HappyCustomer from '../sections/happy-customer';
+import Blog from '../sections/blog';
+import SubscribeUs from '../sections/subscribe-us';
 
-
-const Div = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: right;
-    padding: 16px;
-    padding-right: 140px;
-    padding-left: 820px;
-
-    text-align: right;
-
-    .title {
-      font-size: 64px;
-      font-weight: 600;
-      text-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-
-      @media (max-width: 640px) {
-        font-size: 56px;
-      }
-    }
-
-    .text {
-      margin-top: 30px;
-      font-size: 30px;
-    }
-
-    a {
-      display: block;
-      margin-top: 40px;
-      padding: 14px 42px;
-      text-decoration: none;
-      font-weight: 500;
-      border: none;
-      border-radius: 10px;
-      background: #8e2de2;
-      background: -webkit-linear-gradient(to right, #8e2de2, #4a00e0);
-      background: linear-gradient(to right, #8e2de2, #4a00e0);
-      color: white;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-`;
-
-
-const Home = () => {
+export default function IndexPage() {
   return (
-    <>
-      <Head>
-        <title>Scroop</title>
-      </Head>
-
-      <Div className="landText">
-      
-          <p className="title">نرسم الأفضل لنصنع الأفضل.</p>
-          <p className="text">ألا تريد الأفضل؟!</p>
-          <Link href="/collections">تسوق الان</Link>
-        
-      
-      </Div>
-      <Landing />
-    </>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO
+          title="Startup hosting provider landing"
+          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
+        />
+        <Banner />
+        <Services />
+        <ExcitingFeatures />
+        <WorkHard />
+        <UltimateFeatures />
+        <HappyCustomer />
+        <Blog />
+        <SubscribeUs />
+      </Layout>
+    </ThemeProvider>
   );
-};
-
-export default Home;
+}
